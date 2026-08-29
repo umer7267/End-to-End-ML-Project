e631bd6bf4d1d4ebec59bf6e319dfe3c266770cd
M Umer <umer7267@gmail.com>
2026-08-29 15:52:30 +0200

    Updated Docker Configuration

--- app/api/routes_predict.py
+++ app/api/routes_predict.py
@@ -6,6 +6,7 @@ from app.services.model_service import predict_car_price
 router = APIRouter()
 
 class CarFeatures(BaseModel):
+    company: str
     year: int
     owner: str
     fuel: str
--- app/cache/redis_cache.py
+++ app/cache/redis_cache.py
@@ -1,18 +1,19 @@
 """
 Cache the most common input and output for faster response to API requests
 """
+import os
 import json
 import redis
 from app.core.config import settings
 
-redis_client = redis.Redis.from_url(settings.REDIS_URL)
+REDIS_URL = os.getenv("REDIS_URL")
+
+redis_client = redis.StrictRedis.from_url(REDIS_URL, decode_responses=True)
 
 def get_cached_prediction(key: str):
     value = redis_client.get(key)
+    return eval(value) if value else None
 
-    if value:
-        return json.loads(value)
-    return None
 
-def set_cached_prediction(key: str, value: dict, expire_in: int = 3600):
-    redis_client.setex(key, expire_in, json.dump(value))
+def set_cached_prediction(key: str, value: dict):
+    redis_client.set(key, str(value))
