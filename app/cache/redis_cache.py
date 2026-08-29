f8263d25dcff8e6c0ebf5e3bae213ce481507e68
M Umer <umer7267@gmail.com>
2026-08-29 16:00:00 +0200

    Deployment Settings

--- app/cache/redis_cache.py
+++ app/cache/redis_cache.py
@@ -2,13 +2,10 @@
 Cache the most common input and output for faster response to API requests
 """
 import os
-import json
 import redis
 from app.core.config import settings
 
-REDIS_URL = os.getenv("REDIS_URL")
-
-redis_client = redis.StrictRedis.from_url(REDIS_URL, decode_responses=True)
+redis_client = redis.StrictRedis.from_url(settings.REDIS_URL, decode_responses=True)
 
 def get_cached_prediction(key: str):
     value = redis_client.get(key)
--- app/core/config.py
+++ app/core/config.py
@@ -12,7 +12,7 @@ class Settings:
     API_KEY = os.getenv("API_KEY")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     JWT_ALGORITHM = "HS256"
-    REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
+    REDIS_URL = os.getenv("REDIS_URL")
     MODEL_PATH = "app/models/model.joblib"
 
 settings = Settings()
